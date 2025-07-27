d8fe19175d3e56797d6260cfaf3a384e644d5c22
sneh shah <snehshah1903@gmail.com>
2025-07-27 14:21:55 +0530

    map test 2

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -312,9 +312,10 @@ const Contact = () => {
                     Find Us
                   </h3>
                   <div className="h-64 rounded-lg overflow-hidden">
+                    
                     <iframe
                       title="Divine Yatra Location"
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.0462486211624!2d85.3123291150617!3d27.708955882793074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb198d1c75dfc3%3A0xc74d59fa640d187f!2sPashupatinath%20Temple!5e0!3m2!1sen!2sin!4v1615990022546!5m2!1sen!2sin"
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.020352021949!2d73.22962927384067!3d22.315070042362773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fcf002cb10849%3A0x71fa3ad093e2f8bc!2sSiddheshwar%20plaza!5e0!3m2!1sen!2sin!4v1753606161171!5m2!1sen!2sin"
                       width="100%"
                       height="100%"
                       style={{ border: 0 }}
